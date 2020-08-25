2fe748726f41e035037f00af67b2b5e4d339e16c
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-08-25 20:31:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,7 +137,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages] => "Assembly"
+     programmer_hash[:dennis_ritchie][:languages][1] => "Assembly"
      return programmer_hash
 
 end
